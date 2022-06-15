eac1eb0ad762552f9148c139f0793753f8882f16
EspoTony <espotony98@gmail.com>
2022-06-15 12:45:32 +0200

    fine esercizio

---
+++ src/shop/Prodotto.java
@@ -0,0 +1,28 @@
+package shop;
+
+import java.text.DecimalFormat;
+
+public class Prodotto {
+	  String marca;
+	    String nome;
+	    double prezzo;
+	    double iva;
+
+	    //Costruttore
+	    Prodotto(String brand, String name, double price , double iva) {
+	    marca = brand;
+	    nome = name;
+	    prezzo = price;
+	    this.iva = iva;
+	    }
+	    //Methods
+	    double prezzoFinale() {
+	        return prezzo+((prezzo/100)*iva);
+	    }
+	    String prezzoFormat() {
+	        DecimalFormat df = new DecimalFormat("#.00\u00A4");
+	        String format = df.format(prezzoFinale());
+	        return format;
+	    }
+
+}
